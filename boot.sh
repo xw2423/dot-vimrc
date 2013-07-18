46af6c3b01b0cc489d7173b50e0c92cff2013470
U-xw-laptop\xw <xw@xw-laptop.(none)>
2013-07-18 23:27:53 +0800

    modify protocol

--- boot.sh
+++ boot.sh
@@ -2,21 +2,21 @@
 
 endpath="$HOME/.vim"
 
-echo "start installing\n"
+echo "start installing"
 
 # Backup existing .vim stuff
-echo "backing up current vim config\n"
+echo "backing up current vim config"
 today=`date +%Y%m%d`
 for i in $HOME/.vim $HOME/.vimrc $HOME/.gvimrc; do [ -e $i ] && mv $i $i.$today; done
 
 
-echo "cloning dot-vimrc\n"
-git clone --recursive -b xw http://github.com/xw2423/dot-vimrc.git $endpath
+echo "cloning dot-vimrc"
+git clone --recursive -b xw git://github.com/xw2423/dot-vimrc.git $endpath
 ln -sf $endpath/.vimrc $HOME/.vimrc
 ln -sf $endpath/.gitconfig $HOME
 
 echo "installing vundle"
-git clone http://github.com/gmarik/vundle.git $endpath/bundle/vundle
+git clone git://github.com/gmarik/vundle.git $endpath/bundle/vundle
 
 echo "installing plugins using Vundle"
 vim +BundleInstall! +BundleClean +q
